28237219518709f7c32488cd7508523987338105
Khelsai Rajwade <khelsai@gmail.com>
2023-11-03 20:59:47 +0530

    build

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@
 import './App.css';
 import MainRouter from './AllRoutes/MainRouter';
 import Navbar  from './LandingComponents/Navbar';
-//import { ChakraProvider } from '@chakra-ui/react';
-//import { Footer } from './Components/Footer';
-// import { Navbar } from './AllRoutes/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <Navbar />
       <MainRouter/>
-      {/* <ChakraProvider>
-        <Footer/>
-      </ChakraProvider> */}
     </div>
   );
 }
